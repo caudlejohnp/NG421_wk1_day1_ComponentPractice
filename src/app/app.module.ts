6694f651e06fa6662ac36bcac66b2ec360a01539
caudlejohnp <caudlejohnp@gmail.com>
2020-08-10 20:19:45 -0500

    added compenents for datetime and message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DateTimeComponent } from './date-time/date-time.component';
+import { MessagePreviewComponent } from './message-preview/message-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateTimeComponent,
+    MessagePreviewComponent
   ],
   imports: [
     BrowserModule
