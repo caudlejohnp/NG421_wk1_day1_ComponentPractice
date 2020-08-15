3d96cebdec9fa6db4add64ea40252c3c52c0439b
caudlejohnp <caudlejohnp@gmail.com>
2020-08-15 11:12:12 -0500

    added and completed top and side nav comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { DateTimeComponent } from './date-time/date-time.component';
 import { MessagePreviewComponent } from './message-preview/message-preview.component';
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DateTimeComponent,
     MessagePreviewComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    TopNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
